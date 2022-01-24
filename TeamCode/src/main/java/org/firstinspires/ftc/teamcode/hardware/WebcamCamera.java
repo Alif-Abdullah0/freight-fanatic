05844ca593ff884b43e9c6ceacaee8a559188c56
Alif Abdullah <aabdullah20@stuy.edu>
2022-01-24 16:45:27 -0500

    class changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/WebcamCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/WebcamCamera.java
@@ -1,8 +1,8 @@
-//All this code was heavily aided by this YouTube guide: https://www.youtube.com/watch?v=JO7dqzJi8lw; check it out if you have further questions or difficulty
 package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.hardware.CVPipelines.RingCounter;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -11,24 +11,26 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 //A new class will probably be created for a normal webcam.
 //This code works if you are using a phone as a robot controller, and want to use it as your camera.
-public class PhoneCamera extends Mechanism{
+public class WebcamCamera extends Mechanism{
     //here I initialize the phone camera and the pipeline; there are other ways to do this if you prefer
-    private OpenCvInternalCamera phoneCam;
+    private WebcamName webcamName;
+    private OpenCvCamera camera;
     private RingCounter counter = new RingCounter();
     private boolean flash;
 
     //this basically links variable phoneCam to the actual phone camera; after that, it links phoneCam to the pipeline, counter; I don't know if "link" is the correct term
     public void init(HardwareMap hwMap){
-        int cameraMonitorViewID = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
-        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK,cameraMonitorViewID);
-        phoneCam.openCameraDevice();
-        phoneCam.setPipeline(counter);
-        phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        int cameraMonitorViewId = hwMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hwMap.appContext.getPackageName());
+        //TODO: Fix deviceName, the second parameter passed to hwMap.get function
+        webcamName = hwMap.get(WebcamName.class, "NAME_OF_CAMERA_IN_CONFIG_FILE");
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        camera.setPipeline(counter);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
             @Override
             public void onOpened()
             {
-                phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -39,11 +41,7 @@ public class PhoneCamera extends Mechanism{
         });
     }
 
-    //It toggles the flashlight... OpenCV has some other features utilizing OpenCvInternalCamera
-    public void flash(){
-        phoneCam.setFlashlightEnabled(!flash);
-        flash = !flash;
-    }
+    
     //Returns the percentage of the target color in the target region; go into RingCounter to learn more
     public double regionValue(){
         return counter.regionValue();
